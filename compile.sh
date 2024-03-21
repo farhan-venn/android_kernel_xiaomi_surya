afd46edf726f6383af84845fd0960df875147fe9
farhan-venn <65814996+farhan-venn@users.noreply.github.com>
2024-03-21 20:22:04 +0700

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# CTRL + F FOR FIND AND REPLACE XXX TO YOUR DEVICE OR ANYTHING
+# 
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="RavenKernel$(date '+%Y%m%d-%H%M').zip"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+
+# change DEFCONFIG to you are defconfig name or device codename
+
+DEFCONFIG="surya_defconfig"
+
+# you can set you name or host name(optional)
+
+export KBUILD_BUILD_USER=Niken
+export KBUILD_BUILD_HOST=Farhan
+
+# change TC_DIR(directory) on where you clone clang toolchain
+
+TC_DIR="/home/raven/kernel/clang-17"
+
+# do not modify export PATCH it's been including with TC_DIR
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+	ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-rf" || $1 = "--regen-full" ]]; then
+	make O=out ARCH=arm64 $DEFCONFIG
+	cp out/.config arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated full defconfig at $DEFCONFIG"
+	exit
+fi
+
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+	rm -rf out
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 Image.gz dtb.img dtbo.img 2> >(tee log.txt >&2) || exit $?
+
+kernel="out/arch/arm64/boot/Image.gz"
+dtb="out/arch/arm64/boot/dtb.img"
+dtbo="out/arch/arm64/boot/dtbo.img"
+
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
+		cp -r $AK3_DIR AnyKernel3
+	elif ! git clone -q https://github.com/MobiusPrjkt/AnyKernel3 -b Mobius; then
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
+	fi
+	cp $kernel $dtb $dtbo AnyKernel3
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout Mobius &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi
