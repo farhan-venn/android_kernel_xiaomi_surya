222508f61d4fd1afbb1e140a3e45a7ab21674737
farhan-venn <65814996+farhan-venn@users.noreply.github.com>
2024-03-22 23:31:16 +0700

    Update surya.sh

--- surya.sh
+++ surya.sh
@@ -107,11 +107,6 @@ makekernel() {
 	STRIP=llvm-strip \
 	OBJCOPY=llvm-objcopy \
 	OBJDUMP=llvm-objdump \
-	CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-none-linux-gnu-" \
-	CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-none-linux-gnueabihf-"
-    else
-	    make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE="${KERNELDIR}/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="${KERNELDIR}/gcc32/bin/arm-eabi-"
-    fi
 
     # Check if compilation is done successfully.
     if ! [ -f "${OUTFILE}" ]; then
